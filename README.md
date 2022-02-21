85452cd8e3ee3bf3730b0840a57be83d31725558
AmeeHamz <global.hamzagc@gmail.com>
2022-02-21 15:39:14 +0500

    1st commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Online Payment Made Easy For You.
+
+![Ameehamzpay-SS-1](https://user-images.githubusercontent.com/92137648/154938750-1f518a88-905a-4bfc-b66e-eeb96da973f1.PNG)
---
+++ src/Footer.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-10 mx-auto">
+              <div className="row">
+                <div className="col-6 col-lg-3">
+                  <h2>Company</h2>
+                  <ul>
+                    <li>
+                      <a href="#">Contact us</a>
+                    </li>
+                    <li>
+                      <a href="#">About us</a>
+                    </li>
+                    <li>
+                      <a href="#">Contributors</a>
+                    </li>
+                    <li>
+                      <a href="#">Privacy Policy</a>
+                    </li>
+                    <li>
+                      <a href="#">Terms of Services</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="col-6 col-lg-3">
+                  <h2>Support</h2>
+                  <ul>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="col-6 col-lg-3">
+                  <h2>Services</h2>
+                  <ul>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="col-6 col-lg-3">
+                  <h2>Follow Us</h2>
+                  <div className="row">
+                    <div className="col-3 mx-auto">
+                      <i class="fab fa-facebook-f fontawesome-style"></i>
+                    </div>
+                    <div className="col-3 mx-auto">
+                      <a
+                        href="https://github.com/AmeeHamz"
+                        target="_thapa">
+                        <i class="fab fa-github fontawesome-style"></i>
+                      </a>
+                    </div>
+                    <div className="col-3 mx-auto">
+                      <i class="fab fa-youtube fontawesome-style"></i>
+                    </div>
+                    <div className="col-3 mx-auto">
+                      <i class="fab fa-twitter fontawesome-style"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr />
+              <div className="mt-5">
+                <p className="main-hero-para text-center w-100">
+                  Copyright @ 2021 AmeehamzPay. All rights reserved.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
